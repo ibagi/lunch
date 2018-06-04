3d35f98dbe30d565a70295dada1f7635c06458e1
bagiimre <imre.bagi@dlms.hu>
2018-06-04 22:19:28 +0200

    moment dependency removed

--- src/providers/newyork.js
+++ src/providers/newyork.js
@@ -1,11 +1,17 @@
-const moment = require('moment');
+const cheerio = require('cheerio');
+const { fetchHtml } = require('../utils');
 const ImageRenderer = require('../renderers/image');
 
-const URI = 'http://www.newyorkbistro.hu/files/images/webmenu/webmenu{STARTOFWEEK}.png';
+const URI = 'http://www.newyorkbistro.hu';
 
 const newyork = async () => {
-    const uri = URI.replace('{STARTOFWEEK}', moment().startOf('isoweek').format('MMDD'));
-    await ImageRenderer.render(uri);
+    const rawHtml = await fetchHtml(URI);
+    const $ = cheerio.load(rawHtml);
+    const link = $('a').filter(function() {
+       return $(this).text().toLocaleLowerCase().includes('heti')
+    })[0];
+    
+    await ImageRenderer.render(`${URI}\\${$(link).attr('href')}`);
 }
 
 module.exports = newyork;
--- src/providers/sorarium.js
+++ src/providers/sorarium.js
@@ -1,11 +1,15 @@
-const moment = require('moment');
+const cheerio = require('cheerio');
+const { fetchHtml } = require('../utils');
 const ImageRenderer = require('../renderers/image');
 
-const URI = 'http://sorarium.hu/wp-content/plugins/sorarium/heti-menu/menu_images/{WEEK}_heti_menu.png';
+const URI = 'http://sorarium.hu/gasztronomia/';
 
 const sorarium = async () => {
-    const uri = URI.replace('{WEEK}', moment().format('W'));
-    await ImageRenderer.render(uri);
+    const rawHtml = await fetchHtml(URI);
+    const $ = cheerio.load(rawHtml);
+    const link = $('#heti-menu').find('a')[0];
+    
+    await ImageRenderer.render($(link).attr('href').replace('https', 'http'));
 }
 
 module.exports = sorarium;
